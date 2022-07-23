fc1580452cc376bcc395d3f106dcc5c5c1c2fdda
essthere7 <softwareQA.info@gmail.com>
2022-07-23 17:13:16 -0500

    Lesson8

--- src/main/java/lesson8/App.java
+++ src/main/java/lesson8/App.java
@@ -1,6 +1,7 @@
 package lesson8;
 
 import javax.sound.midi.Soundbank;
+import java.time.Period;
 
 public class App {
     public static void main(String[] args) {
@@ -11,6 +12,22 @@ public class App {
 
 
         System.out.println("Name: " + anna.name + " LastName: " + anna.lastName +  "  Year: " + anna.year);
+
+        Person david = new Person();
+        david.year = 1985;
+        david.name = "David";
+        david.lastName = "Peterson";
+
+        System.out.println("Name: " + david.name + " LastName: " + david.lastName +  "  Year: " + david.year);
+
+
+        Vehicle myCar = new Vehicle();
+        myCar.model = "Camry";
+        myCar.make = " Toyota";
+        myCar.year = 2004;
+        myCar.color = " Red Tesla";
+
+        System.out.println(myCar.color);
 
 
 
---
+++ src/main/java/lesson8/Vehicle.java
@@ -0,0 +1,8 @@
+package lesson8;
+
+public class Vehicle {
+    public String make;
+    public String model;
+    public Integer year;
+    public String color;
+}
